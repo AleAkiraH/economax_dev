9a629cb1af3350cab9be47a60993846866c37f3b
Automações <automacao.desenvolvimento@gmail.com>
2023-05-27 16:23:34 -0300

    removendo alguns conteudos lixose subindo versao com rotas para cadastramento de entradas de proventos.

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def login():
     return funcoes.login(user_crypt,password_crypt)
 #endregion
 
-#region Categorias
+#region Categorias Despesas
 @app.route('/categorias_despesas_geral', methods=['GET'])
 def categorias_despesas_geral():  
     return funcoes.categorias_despesas_geral()
@@ -58,25 +58,61 @@ def busca_categorias_despesas_geral_usuario():
     return funcoes.busca_categorias_despesas_geral_usuario(user_id)
 #endregion
 
+#region Categorias Rendimentos
+@app.route('/categorias_rendimentos_geral', methods=['GET'])
+def categorias_rendimentos_geral():  
+    return funcoes.categorias_rendimentos_geral()
+    
+@app.route('/cadastro_categorias_rendimentos', methods=['POST'])
+def cadastro_categorias_rendimentos():
+    usuario_id = request.json['id_usuario'].lower()
+    nome_categoria = request.json['categoria'].lower()
+    return funcoes.cadastro_categorias_rendimentos(nome_categoria, usuario_id)
+
+@app.route('/cadastro_categorias_rendimentos_usuario', methods=['POST'])
+def cadastro_categorias_rendimentos_usuario():
+    usuario_id = request.json['id_usuario'].lower()
+    categorias = request.json['categorias']
+    return funcoes.cadastro_categorias_rendimentos_usuario(usuario_id,categorias)
+    
+@app.route('/busca_categorias_rendimentos_geral_usuario', methods=['POST'])
+def busca_categorias_rendimentos_geral_usuario():   
+    user_id = request.json['id_usuario'].lower()
+    return funcoes.busca_categorias_rendimentos_geral_usuario(user_id)
+#endregion
+
 #region Relatórios
 @app.route('/ultimas_despesas_usuario', methods=['POST'])
 def ultimas_despesas_usuario():
     usuario_id = request.json['id_usuario'].lower()
-    dias = request.json['dias'].lower()
+    dias = request.json['dias']
     return funcoes.ultimas_despesas_usuario(dias, usuario_id)
     
 @app.route('/ultimas_despesas_usuario_mes_atual_sintetico', methods=['POST'])
 def ultimas_despesas_usuario_mes_atual_sintetico():
     usuario_id = request.json['id_usuario'].lower()
     return funcoes.ultimas_despesas_usuario_mes_atual_sintetico(usuario_id)
+
+@app.route('/gastos_categoria_usuario', methods=['POST'])
+def gastos_categoria_usuario():
+    usuario_id = request.json['id_usuario'].lower()
+    dias = request.json['dias']
+    return funcoes.gastos_categoria_usuario(usuario_id,dias)
+
+@app.route('/soma_total_gastos_por_usuario_por_dia', methods=['POST'])
+def soma_total_gastos_por_usuario_por_dia():
+    usuario_id = request.json['id_usuario'].lower()
+    dias = request.json['dias']
+    return funcoes.soma_total_gastos_por_usuario_por_dia(usuario_id,dias)
 #endregion
     
 #region Cadastro de valores
 @app.route('/cadastro_gastos_usuario', methods=['POST'])
 def cadastro_gastos_usuario():
     usuario_id = request.json['id_usuario'].lower()
-    registros_gastos = request.json['gastos']    
+    registros_gastos = request.json['gastos']
     return funcoes.cadastro_gastos_usuario(registros_gastos,usuario_id)
+
 #endregion
 
 if __name__ == '__main__':
--- funcoes.py
+++ funcoes.py
@@ -3,6 +3,7 @@ import pymongo
 from bson import ObjectId
 from datetime import datetime, timedelta
 import pytz
+import json
 
 client = pymongo.MongoClient("mongodb+srv://administrador:administrador@cluster0.8vjnvh9.mongodb.net/test")
 db = client['Economax']
@@ -54,6 +55,20 @@ def categorias_despesas_geral():
     except Exception as ex:
         return {'Message': 'Um erro ocorreu!', 'Descrição': str(ex)}
 
+def categorias_rendimentos_geral():
+    try:
+        categorias = db.categorias_rendimentos_geral.find()
+        
+        resposta = []
+        
+        for categoria in categorias:
+            dicionario = {"id": str(categoria['_id']),"categoria": categoria['nome']}
+            resposta.append(dicionario)
+
+        return jsonify(resposta)
+    except Exception as ex:
+        return {'Message': 'Um erro ocorreu!', 'Descrição': str(ex)}
+
 def cadastro_categorias(nome_categoria, usuario_id):
     data_hora_formatada = dataNow()
     
@@ -64,6 +79,18 @@ def cadastro_categorias(nome_categoria, usuario_id):
     categoria_id = db.categorias_despesas_geral.insert_one(categoria).inserted_id
     
     categoria_criada = db.categorias_despesas_geral.find_one({'_id': categoria_id}, {'_id': 1, 'nome': 1})
+    return {'id': str(categoria_criada['_id']), 'categoria': categoria_criada['nome']}
+
+def cadastro_categorias_rendimentos(nome_categoria, usuario_id):
+    data_hora_formatada = dataNow()
+    
+    if db.categorias_rendimentos_geral.count_documents({'nome': nome_categoria}) > 0:
+        return {'message': 'Categoria já está cadastrada!'}
+
+    categoria = {'nome': nome_categoria, 'users_id': usuario_id, 'data': data_hora_formatada}
+    categoria_id = db.categorias_rendimentos_geral.insert_one(categoria).inserted_id
+    
+    categoria_criada = db.categorias_rendimentos_geral.find_one({'_id': categoria_id}, {'_id': 1, 'nome': 1})
     return {'id': str(categoria_criada['_id']), 'categoria': categoria_criada['nome']}
 
 def cadastro_categorias_usuario(id_usuario, categorias):
@@ -84,11 +111,44 @@ def cadastro_categorias_usuario(id_usuario, categorias):
         
     except Exception as ex:
         return {'message': 'Um erro ocorreu!', 'descrição': str(ex)}
-   
+    
+def cadastro_categorias_rendimentos_usuario(id_usuario, categorias):
+    try:
+        cadastrar_categorias = db.cadastro_categorias_rendimentos_usuario
+        
+        for categoria in categorias:
+            categoria_id = categoria
+            
+            if cadastrar_categorias.count_documents({'users_id': id_usuario, 'categorias_id': categoria_id['id']}) == 0:
+                nova_categoria = {
+                    "users_id": id_usuario,
+                    "categorias_id": categoria_id['id']
+                }
+                cadastrar_categorias.insert_one(nova_categoria)
 
+        return {'message': 'Categorias inseridas com sucesso!'}
+        
+    except Exception as ex:
+        return {'message': 'Um erro ocorreu!', 'descrição': str(ex)}
+   
 def busca_categorias_despesas_geral_usuario(user_id):
     categorias_usuario = db['cadastrar_categorias_usuario']
     categorias_geral = db['categorias_despesas_geral']
+
+    categorias_user = categorias_usuario.find({'users_id': user_id})
+    categorias = []
+    for categoria in categorias_user:
+        categoria_geral = categorias_geral.find_one({'_id': ObjectId(categoria['categorias_id'])})
+        categorias.append({
+            'id': str(categoria_geral['_id']),
+            'categoria': categoria_geral['nome']
+        })
+
+    return jsonify(categorias)
+
+def busca_categorias_rendimentos_geral_usuario(user_id):
+    categorias_usuario = db['cadastro_categorias_rendimentos_usuario']
+    categorias_geral = db['categorias_rendimentos_geral']
 
     categorias_user = categorias_usuario.find({'users_id': user_id})
     categorias = []
@@ -109,8 +169,9 @@ def cadastro_gastos_usuario(registros_gastos, usuario_id):
      
         categoria_id = registro['id_categoria'].lower()
         valor = registro['valor']
+        descricao = registro['descricao']
         
-        novo_gasto = {'categoria_despesa_id': categoria_id, 'usuario_id': usuario_id, 'valor': valor, 'data': data_hora_formatada}
+        novo_gasto = {'categoria_despesa_id': categoria_id, 'usuario_id': usuario_id, 'valor': valor, 'descricao': descricao, 'data': data_hora_formatada}
 
         gasto_id = gastos.insert_one(novo_gasto)
         
@@ -123,13 +184,21 @@ def ultimas_despesas_usuario(dias, users_id):
         categoria_id = gasto['categoria_despesa_id']
         categoria = db.categorias_despesas_geral.find_one({'_id': ObjectId(categoria_id)})
         resultado.append({
-            'categoria': categoria['nome'],
-            'valor': gasto['valor'].replace(',', ''),
-            'data': gasto['data']
+        'categoria': categoria['nome'],
+        'valor': gasto['valor'].replace(',', ''),
+        'data': gasto['data'],            
+        'descricao': gasto.get('descricao', '')
         })
-    # ajustar
-    data_atual = datetime.now()
-    data_limite = datetime(data_atual.year, data_atual.month, data_atual.day, 0, 0, 0) - timedelta(days=int(dias))
+    
+    if (dias==""):
+        dias = 0
+        data_atual = (datetime.now() - timedelta(days=int(dias))).replace(hour=0, minute=0, second=0)
+        data_limite = data_atual.strftime("%Y-%m-01 00:00:00")
+        data_limite = datetime.strptime(data_limite, '%Y-%m-%d %H:%M:%S')
+    else:
+        data_atual = datetime.now()
+        data_limite = datetime(data_atual.year, data_atual.month, data_atual.day, 0, 0, 0) - timedelta(days=int(dias))
+    
     lista_filtrada = []
 
     for item in resultado:
@@ -152,6 +221,78 @@ def ultimas_despesas_usuario_mes_atual_sintetico(users_id):
 
     return jsonify(resultado)
 
+def gastos_categoria_usuario(usuario_id, dias):
+    collection_gastos = db['gastos']
+    collection_categorias = db['categorias_despesas_geral']
+
+    if (dias == ""):
+        dias = 0
+        data_inicio = (datetime.now() - timedelta(days=int(dias))).replace(hour=0, minute=0, second=0)
+        data_inicio_formatada = data_inicio.strftime("%Y-%m-01 00:00:00")
+    else:
+        data_inicio = (datetime.now() - timedelta(days=int(dias))).replace(hour=0, minute=0, second=0)
+        data_inicio_formatada = data_inicio.strftime("%Y-%m-%d %H:%M:%S")
+
+    query = {
+        'usuario_id': usuario_id,
+        'data': {'$gte': data_inicio_formatada}
+    }
+
+    result = collection_gastos.aggregate([
+        {'$match': query},
+        {'$group': {'_id': '$categoria_despesa_id', 'total': {'$sum': {'$toInt': '$valor'}}}}
+    ])
+
+    soma_gastos_por_categoria = {}
+    for categoria in result:
+        categoria_id = categoria['_id']
+        total = categoria['total']
+        nome_categoria = collection_categorias.find_one({'_id': ObjectId(categoria_id)})['nome']
+        soma_gastos_por_categoria[nome_categoria] = total
+
+    # Construindo o dicionário de resultados
+    resultados = {}
+    for categoria, total in soma_gastos_por_categoria.items():
+        resultados[categoria] = total
+
+    # Convertendo para JSON
+    json_resultados = jsonify(resultados)
+
+    # Exibindo o resultado JSON
+    print(json_resultados)
+    return json_resultados
+
+def soma_total_gastos_por_usuario_por_dia(usuario_id, dias):
+    collection = db['gastos']
+
+    if (dias == ""):
+        dias = 0
+        data_inicio = (datetime.now() - timedelta(days=int(dias))).replace(hour=0, minute=0, second=0)
+        data_inicio_formatada = data_inicio.strftime("%Y-%m-01 00:00:00")
+    else:
+        data_inicio = (datetime.now() - timedelta(days=int(dias))).replace(hour=0, minute=0, second=0)
+        data_inicio_formatada = data_inicio.strftime("%Y-%m-%d %H:%M:%S")
+
+    query = {
+        'usuario_id': usuario_id,
+        'data': {'$gte': data_inicio_formatada}
+    }
+    
+    # Executando a consulta e calculando a soma dos gastos
+    result = collection.aggregate([
+        {'$match': query},
+        {'$group': {'_id': None, 'total': {'$sum': {'$toInt': '$valor'}}}}
+    ])
+
+    # Obtendo o resultado da soma
+    soma_gastos = next(result, {'total': 0})['total']
+
+    data = {
+    "Total": soma_gastos
+    }
+    
+    json_data = jsonify(data)
+    return json_data
 
 # a = cadastro("Alexsander","1234")
 # print(a)
